1c7a8a8e941bcbf1ade55eea049432771e562d7e
jadhavpradeep413 <jadhavpradeep413@gmail.com>
2023-04-07 20:56:33 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SpeechRecognition, {
+  useSpeechRecognition
+} from "react-speech-recognition";
 
-function App() {
+function callApi() {
+  fetch('http://localhost:8080/test2', { method: 'GET',
+  mode: 'no-cors',
+  headers: {
+  'Content-Type': 'text/plain',
+  } })
+    .then(data => data.json()) // Parsing the data into a JavaScript object
+    .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
+}
+ 
+const App = () => {
+  const { transcript, resetTranscript } = useSpeechRecognition({
+    continuous: true
+  });
+  const url = 'http://localhost:8080/test2';
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return null;
+  }
+ 
+
   return (
-    <div className="App">
+    <div>
+      <button onClick={SpeechRecognition.startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+      We got the text <input type="text" value = {transcript}/>
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={callApi}>Call API</button>
       </header>
     </div>
-  );
-}
+    
+      
+    </div>
+    
+  );  
+};
 
 export default App;
---
+++ src/App2.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+
+function callApi() {
+    fetch('http://localhost:8080/test2', { method: 'GET' })
+      .then(data => data.json()) // Parsing the data into a JavaScript object
+      .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
+  }
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={callApi}>Call API</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
