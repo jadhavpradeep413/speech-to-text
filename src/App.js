6570d9fd90721d84b417552c1c7e5626925db671
jadhavpradeep413 <jadhavpradeep413@gmail.com>
2023-04-07 21:44:21 +0300

    commiting vhanges

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
 import SpeechRecognition, {
   useSpeechRecognition
 } from "react-speech-recognition";
 
+
 function callApi() {
-  fetch('http://localhost:8080/test2', { method: 'GET',
-  mode: 'no-cors',
-  headers: {
-  'Content-Type': 'text/plain',
-  } })
+  fetch('http://localhost:8080/test2',{
+    method: "GET",
+    headers: {
+      "access-control-allow-origin" : "*",
+      "Content-type": "application/json; charset=UTF-8"
+    }})
     .then(data => data.json()) // Parsing the data into a JavaScript object
     .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
 }
  
 const App = () => {
+  const [ourText, setOurText] = useState("");
+  const msg = new SpeechSynthesisUtterance();
+  
   const { transcript, resetTranscript } = useSpeechRecognition({
     continuous: true
   });
   const url = 'http://localhost:8080/test2';
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return null;
+  }
+
+
+
+  const speechHandler = (msg) => {
+    msg.text = ourText
+    window.speechSynthesis.speak(msg)
   }
  
 
@@ -34,8 +47,18 @@ const App = () => {
       <header className="App-header">
         <button onClick={callApi}>Call API</button>
       </header>
+
+     
     </div>
-    
+
+     
+    <input
+        type='text'
+        value={ourText}
+        placeholder='Enter Text'
+        onChange={(e) => setOurText(e.target.value)}
+      />
+      <button onClick={() => speechHandler(msg)}>SPEAK</button>
       
     </div>
     
